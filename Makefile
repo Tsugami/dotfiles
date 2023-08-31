5072e75229f31794093e987fd88b0efe7d7c48b8
Yslan Ramos <yslanramos.r@gmail.com>
2023-08-30 23:29:06 -0300

    fix setup script

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ link:
 	ln -s -f $(PWD)/.config/git $(HOME)/.config/git
 	ln -s -f $(PWD)/.zshrc $(HOME)/.zshrc
 	ln -s -f $(PWD)/.p10k.zsh $(HOME)/.p10k.zsh 
+
+setup: link
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing Tsu Dotfiles for the first time in $HOME/.dotfiles"
     git clone --depth=1 git@github.com:Tsugami/dotfiles.git "$HOME/.dotfiles"
     cd "$HOME/.dotfiles"
-    ./bootstrap.sh
+    make setup
 else
     echo ".dotfiles folder already exists. Make sure if it's correctly the repository e run ./bootstrap.sh"
 fi
