62aaee6d8017d046607b63b91cbb934f06dfe98d
Yslan Ramos <yslanramos.r@gmail.com>
2026-02-28 19:27:55 -0300

    Update terragrunt.fish

--- .config/fish/completions/terragrunt.fish
+++ .config/fish/completions/terragrunt.fish
@@ -3,7 +3,7 @@ function __complete_terragrunt
     set -lx COMP_LINE (commandline -cp)
     test -z (commandline -ct)
     and set COMP_LINE "$COMP_LINE "
-    /home/baiacu/.terragrunt/bin/terragrunt
+    $HOME/.terragrunt/bin/terragrunt
 end
 complete -f -c terragrunt -a "(__complete_terragrunt)"
 
